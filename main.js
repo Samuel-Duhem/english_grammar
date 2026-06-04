535551dd3efd04b1918b94842e9343061ebf8c98
Samuel-Duhem <duhemsamuel@gmail.com>
2026-06-04 19:52:13 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 720,
+    minWidth: 700,
+    minHeight: 500,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    title: 'English for Specific Purposes',
+    backgroundColor: '#faf9f6',
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ preload.js
@@ -0,0 +1,1 @@
+// No renderer-to-main IPC needed.
